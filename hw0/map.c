4c03c42fba992fdef427e40fcaba3b3cc6839c42
LeBW <27091925@qq.com>
2019-04-14 06:12:14 +0000

    finish hw0

--- hw0/map.c
+++ hw0/map.c
@@ -10,7 +10,7 @@ int recur(int i) {
     int j = i;
 
     /* TODO 1: Fix this so it prints the address of j */
-    printf("recur call %d: stack@ %p\n", i, j);
+    printf("recur call %d: stack@ %p\n", i, &j);
 
     if (i > 0) {
         return recur(i - 1);
@@ -35,10 +35,10 @@ int main(int argc, char *argv[]) {
     printf("recur @ %p\n", recur);
 
     /* TODO 2: Fix to get address of the stack variable */
-    printf("_main stack: %p\n", i);
+    printf("_main stack: %p\n", &i);
 
     /* TODO 3: Fix to get address of a static variable */
-    printf("static data: %p\n", stuff);
+    printf("static data: %p\n", &stuff);
 
     printf("Heap: malloc 1: %p\n", buf1);
     printf("Heap: malloc 2: %p\n", buf2);
