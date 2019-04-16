64ce4cfcbb3e9df17daeb74c2e31cc4650d7759c
LeBW <27091925@qq.com>
2019-04-16 13:13:15 +0000

    Finish input/output redirection

--- hw1/shell.c
+++ hw1/shell.c
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -164,21 +166,69 @@ int main(unused int argc, unused char *argv[]) {
       /* fork a child process and execute the program. */
       pid_t cpid = fork();
       if (cpid == 0) {
-        size_t length = tokens_get_length(tokens);
-        char* path = tokens_get_token(tokens, 0);
-        char* r_path = resolve_path(path);
-        char* argv[length+1];
-        for (int i = 0; i < length; i++) {
-          argv[i] = tokens_get_token(tokens, i);
-        }
-        argv[length] = NULL;
+          // in child process.
+          size_t length = tokens_get_length(tokens);
+          char* path = tokens_get_token(tokens, 0);
+          char* r_path = resolve_path(path);
+          char* argv[length+1];
+          int i = 0;
+          for (; i < length; i++) {
+              if (strcmp(tokens_get_token(tokens, i), "<") == 0) {
+                  //redirect stdin to the file.
+                  char* file_name = tokens_get_token(tokens, i+1);
+                  if (file_name == NULL)
+                      exit(127);
+                  int in = open(file_name, O_RDONLY);
+                  if (in == -1) {
+                      printf("Error open file: %s\n", file_name);
+                      exit(127);
+                  }
+                  // replace stdin with input file
+                  dup2(in, STDIN_FILENO);
+                  // close unused file descripters.
+                  close(in);
 
-        execv(r_path, argv);
-        exit(0);
+                  for (int j = i; j <= length; j++) {
+                      argv[j] = NULL;
+                  }
+                  execv(r_path, argv);
+                  printf("Error execute the program.\n");
+                  exit(127);
+              }
+              else if (strcmp(tokens_get_token(tokens, i), ">") == 0) {
+                  //redirect stdout to the file.
+                  char* file_name =  tokens_get_token(tokens, i+1);
+                  if (file_name == NULL)
+                      exit(127);
+                  int out = creat(file_name, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                  if (out == -1) {
+                      printf("Error open file: %s\n", file_name);
+                      exit(127);
+                  }
+                  dup2(out, STDOUT_FILENO);
+                  close(out);
+
+                  for (int j = i; j <= length; j++) {
+                      argv[j] = NULL;
+                  }
+                  execv(r_path, argv);
+                  printf("Error execute the program.\n");
+                  exit(127);
+              }
+              else {
+                  argv[i] = tokens_get_token(tokens, i);
+              }
+          }
+          argv[length] = NULL;
+
+          execv(r_path, argv);
+          printf("Error execute the program.\n");
+          exit(0);
       }
       else {
-        int status;
-        wait(&status);
+          // in parent process.
+          int status;
+          wait(&status);
       }
     }
 
--- hw1/test.c
+++ hw1/test.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    char *str = malloc(128);
+    scanf("%s", str);
+    printf("%s\n", str);
     return 0;
 }
